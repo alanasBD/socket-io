f4be65dafa2a0053ce7bc53f2e4b989707613aa3
alanasBD <alanas7171@gmail.com>
2023-01-16 11:47:25 +0600

    namespace

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { Socket } = require("dgram");
 const express = require("express");
 const app = express();
 
@@ -8,8 +9,16 @@ const expressServer = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(expressServer);
 
-io.on("connection", (socket) => {
-  io.sockets.emit("MyEvent", "Hello EveryOne");
+let buyNameSpace = io.of("/buy");
+
+buyNameSpace.on("connection", (socket) => {
+  buyNameSpace.emit("MyEvent", "BuyNameSpace");
+});
+
+let sellNameSpace = io.of("/sell");
+
+sellNameSpace.on("connection", (socket) => {
+  sellNameSpace.emit("MyEvent", "SellNameSpace");
 });
 
 app.get("/", (req, res) => {
