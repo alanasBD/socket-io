4960dab162fa612040c91b19290cbb4350b69a33
alanasBD <alanas7171@gmail.com>
2023-01-16 11:12:23 +0600

    broadcasting

--- index.js
+++ index.js
@@ -2,28 +2,20 @@ const express = require("express");
 const app = express();
 
 const http = require("http");
+
 const expressServer = http.createServer(app);
 
 const { Server } = require("socket.io");
-
 const io = new Server(expressServer);
 
 io.on("connection", (socket) => {
-  console.log("New user connected ...");
-
-  setTimeout(() => {
-    socket.send("Hi,I am from server");
-  }, 5000);
-
-  //   socket.on("disconnect", () => {
-  //     console.log("User disconnected...");
-  //   });
+  io.sockets.emit("MyEvent", "Hello EveryOne");
 });
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-expressServer.listen(3000, () => {
+expressServer.listen(3001, () => {
   console.log("Server is running...");
 });
